ab17de2759b701eed8ea3b5b84d9de66bbc8f9d7
charalampos <charalampos.kokkalis@jesus.ox.ac.uk>
2019-11-16 21:53:48 +0000

    Page with variables and localhost

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
+from chatbot import talk_to_user
+from mood_extraction import sentiment_analysis
+
+text = " ".join(talk_to_user()[0:3])
+textvar = 'Text: ' + text + ' Score: ' + str(sentiment_analysis(text)[0])
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return 'Let\'s get this bread'   
+def home():  
+    return render_template('home.html',var=textvar)
+    
+app.run(host='0.0.0.0', port= 81)
--- mood_extraction.py
+++ mood_extraction.py
@@ -2,8 +2,33 @@
 # This file will conduct sentiment analysis to the user's responses in order to find his mood.
 # Should return vector of emotions
 
+# Imports the Google Cloud client library
+from google.cloud import language
+from google.cloud.language import enums
+from google.cloud.language import types
+
+# local imports
 from chatbot import talk_to_user
 
-mood_related_response = " ".join(talk_to_user()[0:3])
+# other imports
+import os
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'OxHack19-26166b4bf395.json'
 
-print(mood_related_response)
+# mood_related_response = " ".join(talk_to_user()[0:3])
+
+def sentiment_analysis(text):
+
+    # Instantiates a client
+    client = language.LanguageServiceClient()
+
+    # The text to analyze
+    document = types.Document(
+        content=text,
+        type=enums.Document.Type.PLAIN_TEXT)
+
+    # Detects the sentiment of the text
+    sentiment = client.analyze_sentiment(document=document).document_sentiment
+    return [sentiment.score, sentiment.magnitude]
+
+# print('Text: {}'.format(text))
+# print('Sentiment: {}, {}'.format(sentiment.score, sentiment.magnitude))
