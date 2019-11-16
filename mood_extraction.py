7da9aca7afb2f6164d11447ca7fc999aba0d3e50
charalampos <charalampos.kokkalis@jesus.ox.ac.uk>
2019-11-16 16:05:47 +0000

    Added first files for chatbot-mood extraction

---
+++ mood_extraction.py
@@ -0,0 +1,7 @@
+# TODO
+# This file will conduct sentiment analysis to the user's responses in order to find his mood.
+# Should return vector of emotions
+
+from chatbot import talk_to_user
+
+print(talk_to_user())fas
