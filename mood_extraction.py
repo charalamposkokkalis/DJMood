4880232c1f95b0098e3a1babe9f45930820fb9ef
charalampos <charalampos.kokkalis@jesus.ox.ac.uk>
2019-11-16 20:12:47 +0000

    Added code for sentiment analysis

--- mood_extraction.py
+++ mood_extraction.py
@@ -2,8 +2,31 @@
 # This file will conduct sentiment analysis to the user's responses in order to find his mood.
 # Should return vector of emotions
 
+# Imports the Google Cloud client library
+from google.cloud import language
+from google.cloud.language import enums
+from google.cloud.language import types
+
+# local imports
 from chatbot import talk_to_user
+
+# other imports
+import os
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'OxHack19-26166b4bf395.json'
 
 mood_related_response = " ".join(talk_to_user()[0:3])
 
-print(mood_related_response)
+# Instantiates a client
+client = language.LanguageServiceClient()
+
+# The text to analyze
+text = mood_related_response
+document = types.Document(
+    content=text,
+    type=enums.Document.Type.PLAIN_TEXT)
+
+# Detects the sentiment of the text
+sentiment = client.analyze_sentiment(document=document).document_sentiment
+
+print('Text: {}'.format(text))
+print('Sentiment: {}, {}'.format(sentiment.score, sentiment.magnitude))
