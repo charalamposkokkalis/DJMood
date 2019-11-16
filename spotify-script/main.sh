83c3a68a206d89481c157ab9443ad397b528cecd
Giannis Tyrovolas <johntyro@hotmail.com>
2019-11-16 21:32:20 +0000

    keeping legacy bash script

--- spotify-script/main.sh
+++ spotify-script/main.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-plid="5kOan1EgvpdFnI6Q15vWwd";
+plid="5kOan1EgvpdFnI6Q15vWwd"
+token="BQCSkzBJRwYK1oCoEczHntsdqJEifClaEnUhBO88spJIr-n_VPEMALSTG_MnH3QmF3bbC1i-L6FA8nj-Yn1QyrtGI2oDKe4wc-sUWRr5_-EcE3d-NmVzj6V15r4bsaTQGVpP9LTwrk_e"
 targ="https://api.spotify.com/v1/playlists/"$plid"/tracks?fields=items(track.id)"
-curl -X "GET" $targ -H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: Bearer BQA7q-pU25GZmyIbvE3t_9jLPk1un7_-prAYSXJ5BHp_NlYZvTjIevlxlr5ZnxBDrYvG06oM2skfuAI6PR7V3RXCKEbaiqLcnR1AQDftWt5mHKfZiss2qVoVzEXaV-sxW_PQOdHDWUMh" > playlistIDs.txt
+curl -X "GET" $targ -H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: Bearer "$token > playlistIDs.txt
+grep -oE '[[:alnum:]]{22}' playlistIDs.txt > trackIDs.txt
+
+while read p; do
+	curl -X "GET" "https://api.spotify.com/v1/audio-features/"$p -H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: Bearer "$token >> features.txt
+done <trackIDs.txt
