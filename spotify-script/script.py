fd7e068130594638e0e1d4343b34c171700d6a24
IoStamoulis <iostamoulis@gmail.com>
2019-11-17 03:21:12 +0200

    Added weight function

--- spotify-script/script.py
+++ spotify-script/script.py
@@ -21,7 +21,14 @@ lib = [("spotifycharts","37i9dQZEVXbMDoHDwVN2tF")]
 #	valence: 0.0 to 1.0 happiness
 # 	tempo: 
 #	acousticness: acoustic
+#  (happy, angry, excited, sad, scared, bored) each in [0,1]
 
+#  happy -> loudness, energy, speechiness, danceability, valence (!!)
+#  excited -> loudness, energy(!!), speechiness, danceability(!), valence, tempo
+#  angry -> loudness, instrumentalness(!), valence, tempo (slow)
+#  sad -> energy, instrumentalness (!), liveness (?), valence(!!)
+#  scared -> loudness (low), instrumentalness, valence, tempo (low)
+#  bored -> energy(!!), loudness, speechiness, valence, tempo (high)
 
 features = ["danceability", "energy", "key", "loudness", "mode", "speechiness", 
 					"acousticness", "instrumentalness", "liveness", "valence", "tempo"]
@@ -42,10 +49,31 @@ def extractFeat(tr):
 	   feats.append(sp.audio_features(track['id']))
 	return feats;
 
+def suit(song, happy, angry, excited, sad, scared, bored):
+		h1  = happy*(song['loudness'] + song['energy'] + song['speechiness']*0.5 + song['danceability'] + 3*song['valence'] + song['tempo']/150)
+		e1  = excited*(song['loudness'] + 3*song['energy'] + song['speechiness']/2 + 2*song['danceability'] + song['valence'] + song['tempo']/50 )
+		a1  = angry*(-song['loudness']/2 + 2*song['instrumentalness'] + 2*song['valence'] + 200/song['tempo'])
+		sa1 = sad*(song['energy'] + 2*song['instrumentalness'] + 3*song['valence'] + song['danceability']/2 + song['tempo']/150)
+		sc1 = scared*(1/song['loudness'] + song['instrumentalness'] + 2*song['valence'] + 200/song['tempo'])
+		b1  = bored*(3*song['energy'] + 1.5*song['loudness'] + song['speechiness'] + song['valence'] + song['tempo']/50) 
+		return(h1+e1+a1+sa1+sc1+b1)
+
 
 tracks = download();
 features = extractFeat(download());
+ratings = []
+ids_sorted=[]
+for i in range(0,len(features)):
+	ratings.append(suit(features[i][0], 0.251, 0.177, 0.246, 0.138, 0.158, 0.0287))
+	ids_sorted.append((features[i][0])['id'])
+ratings, ids_sorted = (list(x) for x in zip(*sorted(zip(ratings, ids_sorted), key=lambda pair: pair[0])))
+ids_sorted.reverse()
+print(ids_sorted)
+
+
+'''
 features = sorted(features, key = lambda i: i[0]['valence'])
 
 for i in range(0,len(features)):
 	print(features[i][0]['id'])
+'''
