e186b7b5e81def98ff91fba7668b1562d4cd7ce4
charalampos <charalampos.kokkalis@jesus.ox.ac.uk>
2019-11-17 03:36:51 +0000

    Now working online

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request
 from wtforms import Form, StringField, validators
 
-
 # local imports
 from chatbot import talk_to_user
 from mood_extraction import sentiment_analysis
@@ -10,9 +9,6 @@ from mood_extraction import sentiment_analysis
 class InputForm(Form):
     r = StringField(u'The text: ', [validators.length(max=1000)])
 
-# text = " ".join(talk_to_user()[0:3])
-# results = sentiment_analysis(text)
-# print(results)
 
 app = Flask(__name__)
 
@@ -21,24 +17,8 @@ def home():
     form = InputForm(request.form)
     if request.method == 'POST' and form.validate():
         r = sentiment_analysis(form.r.data)
-        # r = form.r.data
-
         return render_template("first.html", form=form, s=r)
     else:
         return render_template("second.html", form=form) 
     
 # app.run(host='0.0.0.0',port=81)
-"""
-
-from flask import Flask
-app = Flask(__name__)
-
-
-@app.route('/')
-def hello():
-    return "Hello World!"
-
-if __name__ == '__main__':
-    app.run()
-
-"""
