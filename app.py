a8ca8e5bd4f9f46905f2c914a5305a91f279ed5c
charalampos <charalampos.kokkalis@jesus.ox.ac.uk>
2019-11-17 00:54:38 +0000

    Lots of stuff

--- app.py
+++ app.py
@@ -1,14 +1,30 @@
-from flask import Flask, render_template
-from chatbot import talk_to_user
+from flask import Flask, render_template, request
+from wtforms import Form, StringField, validators
+
+
+# local imports
+# from chatbot import talk_to_user
 from mood_extraction import sentiment_analysis
 
-text = " ".join(talk_to_user()[0:3])
-textvar = 'Text: ' + text + ' Score: ' + str(sentiment_analysis(text)[0])
+# Model
+class InputForm(Form):
+    r = StringField(u'The text: ', [validators.length(max=1000)])
+
+# text = " ".join(talk_to_user()[0:3])
+# results = sentiment_analysis(text)
+# print(results)
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():  
-    return render_template('home.html',var=textvar)
+@app.route('/',methods=['GET', 'POST'])
+def home():
+    form = InputForm(request.form)
+    if request.method == 'POST' and form.validate():
+        r = sentiment_analysis(form.r.data)
+
+        return render_template("home2.html", form=form, s=r)
+    else:
+        return render_template("home.html", form=form) 
     
-app.run(host='0.0.0.0', port= 81)
+app.run(host='0.0.0.0',port=81)
+
--- mood_extraction.py
+++ mood_extraction.py
@@ -3,6 +3,5 @@ from chatbot import talk_to_user
 
 paralleldots.set_api_key("C4hoTyr6iYapNCizZ1QCvHs99NbJ3A0g4aaWRG50Uow")
 
-text = " ".join(talk_to_user()[0:3])
-
-print(paralleldots.emotion(text))
+def sentiment_analysis(text):
+    return paralleldots.emotion(text)['emotion']
